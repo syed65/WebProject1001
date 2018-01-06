835f5ce0311d4b60034778602b7469db7d4b9045
Syed Almamun <mamun2630@gmail.com>
2018-01-06 10:22:58 -0500

    This is my 3rd commit

--- VariablesTypeCasting/src/com/icterguru/variables/Demo.java
+++ VariablesTypeCasting/src/com/icterguru/variables/Demo.java
@@ -4,16 +4,16 @@ public class Demo {
 	 public static void main(String args[]) {
 	  byte x;
 	  int a = 270;
-	  double b = 128.128;
+	  double b = 128.28;
 	  System.out.println("int converted to byte");
 	  x = (byte) a;
-	  System.out.println("a and x " + a + " " + x);
+	  System.out.println("a= "+a+" and"+ " x= " + x);
 	  System.out.println("double converted to int");
 	  a = (int) b;
-	  System.out.println("b and a " + b + " " + a);
+	  System.out.println("b= "+b+" and"+ " a= " + a);
 	  System.out.println("\n double converted to byte");
 	  x = (byte)b;
-	  System.out.println("b and x " + b + " " + x);
+	  System.out.println("b= "+b+" and"+ " x= " + x);
 	 }
 	}
 
